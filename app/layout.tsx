32d35337ffa7ad74cafbf6b3c249d0275a227f28
Manishhh10 <manishbhatt2121714@gmail.com>
2025-12-27 19:36:32 +0545

    app layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,35 @@
-import "./globals.css";
+import type React from "react"
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+// import { Navbar } from "@/components/layout/navbar"
+
+const _geist = Geist({ subsets: ["latin"] })
+const _geistMono = Geist_Mono({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Nivaas - Host Place or Experience in Nepal",
+  description: "Login, Sign up, or Start hosting on Nivaas",
+  generator: "Next.js",
+  icons: {
+    icon: [
+      {
+        url: "/images/logo.png",
+        type: "image/png",
+      },
+    ],
+    apple: "/images/logo.png",
+  },
+}
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`font-sans antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
