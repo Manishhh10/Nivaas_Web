e5e7dedbc172a4b52f204da41ec42d4cd5aa36a7
Manishhh10 <manishbhatt2121714@gmail.com>
2026-01-20 20:04:34 +0545

    axios and endpoints added

---
+++ lib/api/axios.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL 
+    || 'http://localhost:5000';
+
+const axiosInstance = axios.create(
+    {
+        baseURL: BASE_URL,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        withCredentials: true, // Important for cookies
+    }
+);
+
+export default axiosInstance;
