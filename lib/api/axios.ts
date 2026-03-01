716a736afa53eee02bfbb8daa9b38b6f08c6a7db
Manishhh10 <manishbhatt2121714@gmail.com>
2026-03-01 12:35:15 +0545

    update lib/api/axios.ts

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -6,11 +6,19 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 const axiosInstance = axios.create(
     {
         baseURL: BASE_URL,
-        headers: {
-            'Content-Type': 'application/json',
-        },
         withCredentials: true, // Important for cookies
     }
 );
+
+// Request interceptor to set Content-Type for JSON requests
+// But allow FormData requests to set their own Content-Type (multipart/form-data)
+axiosInstance.interceptors.request.use((config) => {
+  // If there's no data or data is FormData, don't set Content-Type
+  // The browser will set it automatically with the correct boundary
+  if (config.data && !(config.data instanceof FormData)) {
+    config.headers['Content-Type'] = 'application/json';
+  }
+  return config;
+});
 
 export default axiosInstance;
